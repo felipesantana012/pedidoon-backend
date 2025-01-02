2dee28cfae5fc2238a1efb28f1edce95b098b90c
Felipe Santana <felipe012santana012@gmail.com>
2025-01-02 09:06:02 -0300

    feat: add cors para permição do front-end

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import routes from "./routes/index.js";
 import bodyParser from "body-parser";
 import path from "path";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/uploads", express.static(path.join(process.cwd(), "src/uploads")));
